4d049d2d9010f3e3a45dbee0d96cda3690f3ab33
kal2-2 <kal2-2@mrt.kit.edu>
2024-07-15 17:26:36 +0200

    feature: implement state machine

---
+++ src/kal2_control/src/kal2_control/main_controller.py
@@ -0,0 +1,199 @@
+from typing import Any, List, Optional
+from enum import Enum, auto
+from collections import deque
+
+import numpy as np
+
+import rospy
+from tf2_ros import TransformBroadcaster, Buffer, TransformListener, LookupException, ConnectivityException, ExtrapolationException  # type: ignore
+from visualization_msgs.msg import MarkerArray, Marker
+
+from ros_numpy import numpify  # type: ignore
+from kal2_util.node_base import NodeBase
+from kal2_control.state_machine import VehicleStateMachine, VehicleState, Zone, TargetCity, StatesLogger
+
+try:
+    from kal2_srvs.srv import StartDriving
+except ImportError as e:
+    print(e)
+
+
+class TrackGate:
+    def __init__(self, name: str, zone: Zone, gate_position: np.ndarray, radius: float, epsilon: float = 0.3) -> None:
+        self._name = name
+        self._zone = zone
+        self._gate_position = gate_position
+        self._radius = radius
+        self._epsilon = epsilon
+        self._was_inside = False
+
+    @property
+    def inside_gate(self):
+        return self._was_inside
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def zone(self):
+        return self._zone
+
+    def is_inside_gate(self, current_pose: np.ndarray):
+        distance = np.linalg.norm(self._gate_position - current_pose)
+
+        in_inner_circle = distance < (self._radius + self._epsilon)
+        in_outer_circle = distance < self._radius
+
+        if in_inner_circle:
+            self._was_inside = True
+        elif not in_outer_circle:
+            self._was_inside = False
+
+        return self._was_inside
+
+
+class InitialPoseEstimator:
+    def __init__(self, n_poses: int) -> None:
+        self._poses = deque(maxlen=n_poses)
+
+    def append(self, pose):
+        self._poses.append(pose)
+
+    def get_estimate(self):
+        if not self.has_estimate():
+            raise ValueError("No estimate available.")
+
+        poses = np.array(self._poses)
+
+        return np.median(poses, axis=0)
+
+    def has_estimate(self):
+        return len(self._poses) == self._poses.maxlen
+
+    def reset(self):
+        self._poses.clear()
+
+
+class MainControllerNode(NodeBase):
+    def __init__(self) -> None:
+        super().__init__(name="sign_detector_node")
+        rospy.loginfo("Starting main controller node...")
+
+        self._sm = VehicleStateMachine(VehicleState(), listeners=[StatesLogger()], allow_event_without_transition=True)
+
+        self._initial_pose_estimator = InitialPoseEstimator(n_poses=5)
+        radius = 0.5
+        self._track_gates = [
+            TrackGate("LD1", Zone.LaneDetectionZone, np.array([4.0, 0.7]), radius=radius),
+            TrackGate("LD2", Zone.LaneDetectionZone, np.array([0.8, 2.0]), radius=radius),
+            TrackGate("SD1", Zone.SignDetectionZone, np.array([2.0, 4.0]), radius=radius),
+            TrackGate("SD2", Zone.SignDetectionZone, np.array([6.0, 2.2]), radius=radius),
+        ]
+
+        self._tf_buffer = Buffer()
+        self._tf_listener = TransformListener(self._tf_buffer)
+        self._marker_array_publisher = rospy.Publisher("/kal2/gates", MarkerArray, queue_size=10, latch=True)
+
+        self._start_service = rospy.Service("start_driving", StartDriving, self._start_driving_handler)
+        self._timer = rospy.Timer(rospy.Duration.from_sec(0.1), callback=self._loop_callback)
+
+        rospy.loginfo("Main controller node started.")
+
+    @property
+    def vehicle_state(self) -> VehicleState:
+        return self._sm.model  # type: ignore
+
+    @property
+    def current_state(self):
+        return self._sm.current_state
+
+    @property
+    def current_zone(self) -> Optional[Zone]:
+        return self.vehicle_state.current_zone
+
+    def _publish_gate_markers(self):
+        from std_msgs.msg import ColorRGBA, Header
+        from geometry_msgs.msg import Vector3, Quaternion, Point
+
+        marker_array = MarkerArray()
+
+        for i, gate in enumerate(self._track_gates):
+            x, y = tuple(gate._gate_position)
+            radius = gate._radius
+
+            marker = Marker()
+            marker.header = Header(frame_id="stargazer", stamp=rospy.Time.now())
+            marker.ns = "spheres"
+            marker.id = i
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
+            marker.pose.position = Point(x=x, y=y, z=0)
+            marker.pose.orientation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
+            marker.scale = Vector3(x=2 * radius, y=2 * radius, z=0.01)
+            marker.color = ColorRGBA(r=1.0, g=0.0, b=0.0, a=1.0)
+
+            marker_array.markers.append(marker)
+
+        self._marker_array_publisher.publish(marker_array)
+
+    def _start_driving_handler(self, msg):
+        try:
+            target_city = TargetCity(msg.target_city)
+        except ValueError as e:
+            rospy.logerr(e)
+            return False
+
+        self._sm.start_driving(target_city=target_city)
+
+        return True
+
+    def _detect_current_zone(self, current_pose):
+        current_position = current_pose[:2, 3]
+
+        if self.current_zone is None:
+            raise ValueError(f"Current zone should not be None in state {self.current_state}.")
+
+        for gate in self._track_gates:
+            was_inside_gate = gate.inside_gate
+            is_inside_gate = gate.is_inside_gate(current_position)
+
+            if is_inside_gate and not was_inside_gate:
+                if self.current_zone == Zone.RecordedZone:
+                    rospy.loginfo(f"Passed gate: {gate.name}, entering zone: {gate.zone} from {self.current_zone}.")
+                    self._sm.zone_detected(gate.zone)
+                else:
+                    rospy.loginfo(f"Passed gate: {gate.name}, entering zone: {Zone.RecordedZone} from {gate.zone}.")
+                    self._sm.zone_detected(Zone.RecordedZone)
+
+                break
+
+    def _loop_callback(self, _):
+        self._publish_gate_markers()
+
+        try:
+            transform = self._tf_buffer.lookup_transform(
+                source_frame="vehicle_rear_axle", target_frame="stargazer", time=rospy.Time(0)
+            )
+        except (LookupException, ConnectivityException, ExtrapolationException) as e:
+            rospy.logwarn(e)
+            return
+
+        current_pose = numpify(transform.transform)
+
+        if self.current_state == VehicleStateMachine.initializing:
+            angle = np.arctan2(current_pose[1, 0], current_pose[0, 0])
+            pose2d = np.hstack((current_pose[:2, 3], angle))
+            self._initial_pose_estimator.append(pose2d)
+
+            if self._initial_pose_estimator.has_estimate():
+                estimate = self._initial_pose_estimator.get_estimate()
+                rospy.loginfo(f"Initial pose: {estimate}")
+                self._sm.initial_pose_detected(initial_pose=estimate)
+
+        if self.current_state in [
+            VehicleStateMachine.in_sign_detection_zone,
+            VehicleStateMachine.in_lane_detection_zone,
+            VehicleStateMachine.in_recorded_zone,
+        ]:
+            self._detect_current_zone(current_pose)
